b473117d710ccc9e2333d02b019e2fe8c88faf71
Małgorzata Lewandowska <117546399+Margaret716@users.noreply.github.com>
2023-05-21 16:44:17 +0200

    Update FIFO.py

--- FIFO.py
+++ FIFO.py
@@ -1,18 +1,18 @@
 
 def FIFO_algorithm(reference_string):
 
-    queue = [0] * 3     #tworzenie 3 elementowej tablicy -równoznaczne z queue = [0, 0, 0]
-    FIFO_hits = 0       #ustalam wartość początkową dla wartości hit
+    queue = [0] * 3     #creating label with 3 elements
+    FIFO_hits = 0       
 
-    for ref_val in reference_string:    #loopujemy przez listę liczb, które funkcja dostaje jako argument
+    for ref_val in reference_string:    #loop the list
         if ref_val in queue:
-            FIFO_hits += 1              #jeśli napotkana wartość znajduje się już w liście dodajemy 1 do warości hit
-            continue                    #i wracamy do linii 7
+            FIFO_hits += 1              # If the encountered value is already in the list, we add 1 to the "hit" value
+            continue                    # we return to 7th line
 
-        queue[2] = queue[1]             #tutaj ustawiamy kolejność konkretnych liczb względem algorytmu FIFO
+        queue[2] = queue[1]             #setting numbers in FIFO algorithm order
         queue[1] = queue[0]
         queue[0] = ref_val
 
-    missing_FIFO = len(reference_string) - FIFO_hits    #obliczamy liczbę missingów i zapisujemy wynik do pliku
+    missing_FIFO = len(reference_string) - FIFO_hits    #calculating missings number and saving it to the file
     with open("results/FIFO_results.csv", "a") as w:
         w.write(f"{missing_FIFO} ")
