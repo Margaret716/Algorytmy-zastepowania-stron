42dbb3bef2d24b0ee05ad049773a427b96277ef9
Małgorzata Lewandowska <117546399+Margaret716@users.noreply.github.com>
2023-05-21 16:16:51 +0200

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -3,12 +3,12 @@ import random
 class Dane:
 
     def __init__(self):
-        """inicjujemy dane klasy"""
+        """Initialization of class data"""
 
-    def stworz_losowe_dane(self):
+    def create_random_data(self):
 
-        file_to_delete = open("results/LRU_results.csv", "w")   #usuwamy pozostałości w plikach przechowujących wyniki
-        file_to_delete.close()                                  #po poprzednim uruchomieniu programu
+        file_to_delete = open("results/LRU_results.csv", "w")   #  removing remnants of the files from the previous run
+        file_to_delete.close()                                 
 
         file_to_delete = open("results/FIFO_results.csv", "w")
         file_to_delete.close()
@@ -16,8 +16,8 @@ class Dane:
         file_to_delete = open("results/results.csv", "w")
         file_to_delete.close()
 
-        for number in range(1, 51):                             #tworzymy 50 plików w folderze data o nazwach 1-50.csv
+        for number in range(1, 51):                             #creating 50 files in directory "data"
             with open(f"data/{number}.csv", "w") as f:
-                for _ in range(0, 100):                         #zapisujemy do nich 100 losowych liczb z zakresu 1-15 jedna pod drugą
+                for _ in range(0, 100):                         #saving in each file 100 random numbers from 1 to 15
                     f.write(f"{random.randint(1, 15)}\n")
                 f.close()
